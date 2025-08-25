6b29958bf3e160ed3bb6f884b2c10d10917e3a39
JTrumble21 <jessi.oostrom@triosstudent.com>
2025-08-25 01:14:51 -0400

    added server files and config files

--- reservations/src/App.js
+++ reservations/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-
+import Navbar from './components/navigation';
+import CreateReservation from './components/CreateReservation';
+// import Post from './components/Reservation';
+// import PostList from './components/ReservationList';
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path={"/create-reservation"} element={<CreateReservation/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
+ 
 export default App;
